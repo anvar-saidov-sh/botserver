f571615828c8b47a205eaeb53accf2f972118011
anvarbeck <anvarsaidov732@gmail.com>
2025-11-19 16:01:53 +0500

    removal of express

--- server.mjs
+++ server.mjs
@@ -7,9 +7,9 @@ dotenv.config();
 // const app = express()
 // const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  res.send("Bot is running")
-})
+// app.get("/", (req, res) => {
+//   res.send("Bot is running")
+// })
 
 // app.listen(PORT, ()=> console.log("Server is running on port ${PORT}"))
 
